bcaa167471ecd661c0de0adf60b811881dbebc35
ow3ndesu <enguerra1111@gmail.com>
2025-04-23 08:58:44 +0800

    change way of printing changelog

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Set date format and output file
-DATE=$(date +'%Y-%m-%d')
-VERSION=${1:-"Unreleased"}
-FILE=CHANGELOG.md
+echo "## [Unreleased] - $(date +'%Y-%m-%d')" > CHANGELOG.md
+echo "" >> CHANGELOG.md
 
-# Write header if it doesn't exist
-if [ ! -f "$FILE" ]; then
-  echo -e "# Changelog\n\nAll notable changes will be documented in this file.\n" > "$FILE"
-fi
-
-# Append the latest changes
-{
-  echo -e "## [$VERSION] - $DATE\n"
-  git log --pretty=format:"- %s (%h)" $(git describe --tags --abbrev=0)..HEAD
-  echo -e "\n"
-} >> "$FILE"
+# List all commits since last tag (or all if no tags exist)
+git log --pretty=format:"- %s (%an)" >> CHANGELOG.md
