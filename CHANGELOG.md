e7aa1796f25dbc38da1a217d11d48262b7e63560
ow3ndesu <enguerra1111@gmail.com>
2025-04-23 09:04:59 +0800

    feat: added boilerplate

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-## [Unreleased] - 2025-04-23
-
-- added boilerplate (ow3ndesu)
--- generate-changelog.sh
+++ generate-changelog.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-echo "## [Unreleased] - $(date +'%Y-%m-%d')" > CHANGELOG.md
-echo "" >> CHANGELOG.md
+DATE=$(date +'%Y-%m-%d')
+VERSION=${1:-"Unreleased"}
+FILE=CHANGELOG.md
 
-# List all commits since last tag (or all if no tags exist)
-git log --pretty=format:"- %s (%an)" >> CHANGELOG.md
+# Write header if it doesn't exist
+if [ ! -f "$FILE" ]; then
+  echo -e "# Changelog\n\nAll notable changes will be documented in this file.\n" > "$FILE"
+fi
+
+# Get the latest tag, or use empty string if no tags exist
+LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "")
+
+# Set the commit range
+if [ -z "$LATEST_TAG" ]; then
+  RANGE=""
+else
+  RANGE="$LATEST_TAG..HEAD"
+fi
+
+# Append changes
+{
+  echo -e "## [$VERSION] - $DATE\n"
+  git log --pretty=format:"- %s (%h)" $RANGE
+  echo -e "\n"
+} >> "$FILE"
